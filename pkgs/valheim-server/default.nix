17dc33e5f125b493f0ffae46b451869c556a2703
SoarinFerret <33401067+SoarinFerret@users.noreply.github.com>
2025-04-13 12:48:14 -0500

    update manifest id

--- pkgs/valheim-server/default.nix
+++ pkgs/valheim-server/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation (finalAttrs: {
     inherit (finalAttrs) name;
     appId = "896660";
     depotId = "896661";
-    manifestId = "6248571692813894409"; # https://steamdb.info/depot/896661/manifests/
+    manifestId = "18007466826975597"; # https://steamdb.info/depot/896661/manifests/
     hash = "sha256-dz2mpv1pVku4XkOio5la/QOM7nTw72LiKQuQSffB+Bc=";
   };
 
