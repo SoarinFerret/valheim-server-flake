d59d610bd5c0ffce8d215ea55b42281ecfa9fa4d
Soarinferret <thecodye@kanto.cloud>
2025-11-01 12:22:43 -0500

    testing new version

--- pkgs/valheim-server/default.nix
+++ pkgs/valheim-server/default.nix
@@ -10,8 +10,8 @@ stdenv.mkDerivation (finalAttrs: {
     inherit (finalAttrs) name;
     appId = "896660";
     depotId = "896661";
-    manifestId = "18007466826975597"; # https://steamdb.info/depot/896661/manifests/
-    hash = "sha256-dz2mpv1pVku4XkOio5la/QOM7nTw72LiKQuQSffB+Cc=";
+    manifestId = "3582760691865144507"; # https://steamdb.info/depot/896661/manifests/
+    hash = "sha256-8UdoLzKiu8CEztqwTHGP5M3RdrrVUTmAwN6Cqt9R+w1=";
   };
 
   # Skip phases that don't apply to prebuilt binaries.
