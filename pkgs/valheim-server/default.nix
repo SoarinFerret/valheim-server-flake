67e2b23a2b0a52ee6b213df39ac44d04a6e9024f
Soarinferret <thecodye@kanto.cloud>
2025-11-01 12:59:18 -0500

    try older build

--- pkgs/valheim-server/default.nix
+++ pkgs/valheim-server/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation (finalAttrs: {
     inherit (finalAttrs) name;
     appId = "896660";
     depotId = "896661";
-    manifestId = "3580572581606309980"; # https://steamdb.info/depot/896661/manifests/
+    manifestId = "8626142906162840635"; # https://steamdb.info/depot/896661/manifests/
     hash = "sha256-8UdoLzKiu8CEztqwTHGP5M3RdrrVUTmAwN6Cqt9R+w1=";
   };
 
